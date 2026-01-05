4d155abfd17843644fd4e934c6aefbaf87cd5e38
favian0226 <favianzming@gmail.com>
2026-01-05 13:33:52 +0800

    Done code.php

--- customer_guest/code.php
+++ customer_guest/code.php
@@ -3,25 +3,55 @@
 session_start();
 include_once __DIR__ . "/includes/dbcon.php";
 
-<?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require 'vendor/autoload.php'; // Adjust the path as needed if you're not using Composer
+require 'vendor/autoload.php'; 
 
 function sendemail_verify($name,$email,$verify_token)
 {
+    $mail = new PHPMailer(true);
+    // $mail->SMTPDebug = 2;
+
+    $mail->isSMTP();
+    $mail->SMTPAuth = true;
     
+    $mail->Host = "smtp.gmail.com";
+    $mail->Username = "favian@gmail.com";
+    $mail->Password = "favian@12345";
+    
+    $mail->SMTPSecure = "tls";
+    $mail->Port = 587;
+    
+    $mail->setFrom("favian@gmail.com", $name);
+    $mail->addAddress($email);
+    
+    $mail->isHTML(true);
+    $mail->Subject = "Email Verification from Funda of Web IT";
+    
+    $email_template = "
+    <h2>You have Registered with Funda of Web IT</h2>
+    <h5>Verify your email address to Login with the below given link</h5>
+    <br/><br/>
+    <a href='http://localhost/fundaofwebit/register-login-with-verification/verify-email.php?token=$verify_token'>Click Me</a>
+    ";
+    
+    $mail->Body = $email_template;
+    $mail->send();
+
+    $mail->Body = $email_template;
+    $mail->send();
+    echo 'Message has been sent';
 }
 
 if(isset($_POST['register_btn']))
 {
     $name = $_POST['name'];
-    $name = $_POST['phone'];
-    $name = $_POST['email'];
-    $name = $_POST['password'];
-    $name = $_POST['confirm_password'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
     $verify_token = md5(rand());
 
     // Email  Exist or not
