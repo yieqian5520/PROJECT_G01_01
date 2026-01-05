66b8438dceb6e09ef14c9e5d44411a4ede9ed8b7
favian0226 <favianzming@gmail.com>
2026-01-05 16:40:01 +0800

    Done verify

---
+++ customer_guest/verify-email.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+include_once __DIR__ . "/dbcon.php";
+
+if(isset($_GET['token']))
+{
+    $token = $_GET['token'];
+    $verify_query = "SELECT verify_token, verify_status FROM users WHERE verify_token='$token' LIMIT 1";
+    $verify_query_run = mysqli_query($con, $verify_query);
+
+    if(mysqli_num_rows($verify_query_run) > 0)
+    {
+        $row = mysqli_fetch_array($verify_query_run);
+        if($row['verify_status'] == "0")
+        {
+            $clicked_token = $row["verify_token"];
+            $update_query = "UPDATE users SET verify_status='1' WHERE verify_token='$clicked_token' LIMIT 1";
+            $update_run = mysqli_query($con, $update_query);
+
+            if($update_query_run)
+            {
+                $SESSION['status'] = "Your Account has been verified successfully!";
+                header("Location: login.php");
+                exit(0);
+            }
+            else
+            {
+                $SESSION["status"] = "Verified Failed!";
+                header("Location: login.php");
+                exit(0);
+            }
+        }
+    }
+    else
+    {
+        $_SESSION['status'] = "This token does not exist";
+        header("Location: login.php");
+        exit(0);
+    }
+}
+else
+{
+    $_SESSION['status'] = "Not allowed";
+    header("Location: login.php");
+}
+?>
