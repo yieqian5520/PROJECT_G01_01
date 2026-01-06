49c25d5abbd576c0b8c519d4243b059e90932cbe
favian0226 <favianzming@gmail.com>
2026-01-06 23:09:23 +0800

    Insert contact

--- customer_guest/contact.php
+++ customer_guest/contact.php
@@ -3,7 +3,22 @@
 include_once __DIR__ . "/includes/header.php";
 ?>
 
+<section id="contact" class="contact container">
 
+    <h2 class="section-title">Contact Us</h2>
+
+    <div class="row">
+
+    <form action="" method="POST">
+        <h3>Tell Us Something</h3>
+        <input type="text" placeholder="Your Name" required>
+        <input type="phone" placeholder="Your Phone" required>
+        <input type="email" placeholder="Your Email" required>
+        <textarea placeholder="Your Message..." required></textarea>
+        <button type="submit" class="btn">Send Message</button>
+    </form>
+</div>
+</section>
 
 
 
